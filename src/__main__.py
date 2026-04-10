2ae8c1cbb818a6e0851467d8afe03ffd59b29f09
Felix Lauwerie <flauweri@z2r3p1.42lyon.fr>
2026-04-10 19:45:18 +0200

    you must test if not func and if not args in main

--- src/__main__.py
+++ src/__main__.py
@@ -21,18 +21,26 @@ def main() -> None:
 
     llm = Small_LLM_Model(model_name="Qwen/Qwen3-0.6B")
     results: list[dict[str, Any]] = []
-
     function_finder = FunctionFinder(llm=llm, functions=functions)
+
     for prompt in prompts:
         func = function_finder.searching_function(prompt.prompt)
         if not func:
-            print(f"prompt: {prompt.prompt} (function not found)")
+            results.append({"prompt": prompt.prompt,
+                            "name": "(function not found)",
+                            "parameters": "(parameter(s) not found)"})
+            continue
 
         for function in functions:
             if function.name == func:
                 arguments_finder = ArgsFinder(
                     llm=llm, function=function, prompt=prompt.prompt)
+
         args = arguments_finder.searching_args()
+
+        if not args:
+            args = {"argument(s)": "not found"}
+
         result = {"prompt": prompt.prompt,
                   "name": arguments_finder.function.name,
                   "parameters": args}
